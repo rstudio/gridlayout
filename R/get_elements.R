99ed75d97f2897ec79ae71b0d143b2c006b065b9
Nick Strayer <nick.strayer@gmail.com>
2021-02-17 17:35:32 -0500

    Added method to get elements and their positions out of the layout
    Used for the grid layout app

---
+++ R/get_elements.R
@@ -0,0 +1,35 @@
+#' Get elements and positions out of a grid layout
+#'
+#' @param layout Object of class `"gridlayout"`.
+#'
+#' @return List of all unique elements in the layout and their `id`,
+#'   `{start,end}_row`, and `{start,end}_col`. Positions are indexed starting at
+#'   1
+#'
+#' @examples
+#' @export
+get_elements <- function(layout){
+  UseMethod("get_elements")
+}
+
+#' @export
+get_elements.default <- function(layout){
+  warning("The get_elements function is only defined for objects of the gridlayout class")
+}
+
+#' @export
+get_elements.gridlayout <- function(layout){
+  lapply(
+    unique(c(layout)),
+    function(id){
+      all_pos <- which(layout == id, arr.ind = TRUE)
+      list(
+        id = id,
+        start_row = min(all_pos[,"row"]),
+        end_row = max(all_pos[,"row"]),
+        start_col = min(all_pos[,"col"]),
+        end_col = max(all_pos[,"col"])
+      )
+    }
+  )
+}
---
+++ tests/testthat/test-get_elements.R
@@ -0,0 +1,22 @@
+test_that("multiplication works", {
+
+  grid_obj <- md_to_gridlayout(
+    layout_table = "
+      |      |120px   |1fr    |1fr    |
+      |:-----|:-------|:------|:------|
+      |100px |header  |header |header |
+      |1fr   |sidebar |plot_a |plot_a |
+      |1fr   |sidebar |plot_b |plot_c |"
+  )
+
+  expect_equal(
+    get_elements(grid_obj),
+    list(
+      list(id = "header",  start_row = 1, end_row = 1, start_col = 1, end_col = 3),
+      list(id = "sidebar", start_row = 2, end_row = 3, start_col = 1, end_col = 1),
+      list(id = "plot_a",  start_row = 2, end_row = 2, start_col = 2, end_col = 3),
+      list(id = "plot_b",  start_row = 3, end_row = 3, start_col = 2, end_col = 2),
+      list(id = "plot_c",  start_row = 3, end_row = 3, start_col = 3, end_col = 3)
+    )
+  )
+})
