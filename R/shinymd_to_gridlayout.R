b1e1574f46ed714cb4ddf79ece310919d2838cd9
Nick Strayer <nick.strayer@gmail.com>
2021-02-22 10:12:22 -0500

    Added function to update the gridlayout chunk of a shinymd file

--- R/shinymd_to_gridlayout.R
+++ R/shinymd_to_gridlayout.R
@@ -7,19 +7,72 @@
 #' @export
 #'
 #' @examples
+#'
+#' my_app_loc <- system.file("sample_apps/my_app.Rmd", package = "gridlayout")
+#' my_layout <- shinymd_to_gridlayout(my_app_loc)
+#' my_layout
+#'
 shinymd_to_gridlayout <- function(path_to_shinymd){
 
   shinymd_txt <- paste(readLines(path_to_shinymd), collapse = "\n")
 
   layout_table <- stringr::str_extract(
     shinymd_txt,
-    stringr::regex(
-      "(?<=\\`\\`\\` \\{\\.layout \\.grid\\}).+?(?=\\`\\`\\`)",
-      multiline = TRUE,
-      dotall = TRUE
-    )
+    find_layout_regex
   )
 
   md_to_gridlayout(layout_table)
 }
 
+#' Update a layout chunk in a shinymd document
+#'
+#' @inheritParams shinymd_to_gridlayout
+#' @param new_layout An updated `gridlayout` object to replace the current one
+#' @param updated_shinymd_path Where the shinymd with the new layout should be
+#'   placed. Default is overwriting the previous file
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#'
+#' loc_of_shinymd <- system.file("sample_apps/my_app.Rmd", package = "gridlayout")
+#' first_layout <- shinymd_to_gridlayout(loc_of_shinymd)
+#' first_layout
+#'
+#' new_layout <- md_to_gridlayout(
+#'   layout_table = "
+#'     |      |120px   |1fr    |1fr    |
+#'     |:-----|:-------|:------|:------|
+#'     |100px |header  |header |header |
+#'     |1fr   |sidebar |plot_a |plot_c |
+#'     |1fr   |sidebar |plot_b |plot_b |"
+#' )
+#'
+#' loc_of_new_shinymd <- tempfile(fileext = ".rmd")
+#' update_shinymd(loc_of_shinymd, new_layout, loc_of_new_shinymd)
+#' shinymd_to_gridlayout(loc_of_new_shinymd)
+#'
+update_shinymd <- function(path_to_shinymd, new_layout, updated_shinymd_path = path_to_shinymd){
+
+  shinymd_txt <- paste(readLines(path_to_shinymd), collapse = "\n")
+
+  new_shinymd <- stringr::str_replace(
+    shinymd_txt,
+    find_layout_regex,
+    paste0("\n", to_md(new_layout), "\n")
+  )
+
+  message("Saving shinymd with updated layout to ", updated_shinymd_path)
+  writeLines(
+    new_shinymd,
+    updated_shinymd_path
+  )
+}
+
+
+find_layout_regex <- stringr::regex(
+  "(?<=\\`\\`\\` \\{\\.layout \\.grid\\}).+?(?=\\`\\`\\`)",
+  multiline = TRUE,
+  dotall = TRUE
+)
--- tests/testthat/test-shinymd_to_gridlayout.R
+++ tests/testthat/test-shinymd_to_gridlayout.R
@@ -34,3 +34,29 @@ More text in the sidebar."
 |1fr  |sidebar |main |"
   )
 })
+
+
+test_that("updating layout chunk works", {
+
+  loc_of_shinymd <- system.file("sample_apps/my_app.Rmd", package = "gridlayout")
+  first_layout <- shinymd_to_gridlayout(loc_of_shinymd)
+
+  new_layout <- md_to_gridlayout(
+    layout_table = "
+      |      |120px   |1fr    |1fr    |
+      |:-----|:-------|:------|:------|
+      |100px |header  |header |header |
+      |1fr   |sidebar |plot_a |plot_c |
+      |1fr   |sidebar |plot_b |plot_b |"
+  )
+
+  loc_of_new_shinymd <- tempfile(fileext = ".rmd")
+  update_shinymd(loc_of_shinymd, new_layout, loc_of_new_shinymd)
+  updated_layout <- shinymd_to_gridlayout(loc_of_new_shinymd)
+
+  # Updated layout should be the same as the one we passed in
+  expect_equal(new_layout, updated_layout)
+
+  # Which should, of course, not be the same as the original layout
+  expect_false(to_md(first_layout) == to_md(updated_layout))
+})
