afebe9e0e607f8acfa9765f59f4c8ff4d73140f6
Nick Strayer <nick.strayer@gmail.com>
2021-02-22 12:49:49 -0500

    Griditor addin now finds a layout table if it's present

--- R/griditor_addin.R
+++ R/griditor_addin.R
@@ -1,54 +1,71 @@
-library(shiny)
-library(miniUI)
-
 #' Edit a grid layout as selected
 #'
 #' @return
-#' @export
-#'
-#' @examples
+#' @export NULL
 griditor_addin <- function() {
   requireNamespace("miniUI", quietly = TRUE)
   requireNamespace("shiny", quietly = TRUE)
 
   editor_selection <- rstudioapi::getSourceEditorContext()
+
+  app_editor_id <- editor_selection$id
   selected_text <- editor_selection$selection[[1]]$text
   selected_range <- editor_selection$selection[[1]]$range
 
-  my_layout <- if(selected_text == "") new_gridlayout() else md_to_gridlayout(selected_text)
+  # Try and find layout chunk in document
+  start_of_layout_chunk_index <- which(grepl("\\`\\`\\` \\{\\.layout \\.grid\\}", editor_selection$contents))[1] + 1
+  has_layout_chunk <- length(start_of_layout_chunk_index) != 0
+
+  user_has_selected_text <- selected_text != ""
+
+  if((!user_has_selected_text) & has_layout_chunk){
+    print("Selecting chunk for user")
+    chunk_deliniators <- which(grepl("\\`\\`\\`", editor_selection$contents))
+    end_of_layout_chunk_index <- chunk_deliniators[chunk_deliniators > start_of_layout_chunk_index][1]
+    selected_range <- rstudioapi::document_range(
+      start = rstudioapi::document_position(row = start_of_layout_chunk_index, column = 1),
+      end = rstudioapi::document_position(row = end_of_layout_chunk_index - 1, column = Inf)
+    )
+
+    selected_text <- paste(
+      editor_selection$contents[(start_of_layout_chunk_index): (end_of_layout_chunk_index-1)],
+      collapse = "\n"
+    )
+
+    rstudioapi::setSelectionRanges(selected_range, id = app_editor_id)
+  }
+
+  my_layout <- if(user_has_selected_text | has_layout_chunk) gridlayout::md_to_gridlayout(selected_text) else gridlayout::new_gridlayout()
 
   starting_elements <- gridlayout::get_elements(my_layout)
 
-
-  cat("Selected layout!\n\n", my_layout)
-
-  ui <- tags$body(
+  ui <- shiny::tags$body(
     rmarkdown::html_dependency_font_awesome(),
-    tags$head(
-      tags$link(rel="preconnect", href="https://fonts.gstatic.com"),
-      tags$link(href="https://fonts.googleapis.com/css2?family=Comic+Neue&display=swap", rel="stylesheet"),
+    shiny::tags$head(
+      shiny::tags$link(rel="preconnect", href="https://fonts.gstatic.com"),
+      shiny::tags$link(href="https://fonts.googleapis.com/css2?family=Comic+Neue&display=swap", rel="stylesheet"),
     ),
-    includeScript(
+    shiny::includeScript(
       system.file("griditor/griditor.js", package = "gridlayout")
     ),
-    includeCSS(
+    shiny::includeCSS(
       system.file("griditor/styles.css", package = "gridlayout")
     ),
-    div(id = "header",
-        h2("Griditor: Build a grid layout for your Shiny app"),
-        actionButton("get_code", "Update selected layout")
+    shiny::div(id = "header",
+               shiny::h2("Griditor: Build a grid layout for your Shiny app"),
+               shiny::actionButton("get_code", "Update selected layout")
     ),
-    div(
+    shiny::div(
       id = "settings",
-      h3(icon("cog"), "Settings"),
-      div(
+      shiny::h3(shiny::icon("cog"), "Settings"),
+      shiny::div(
         class = "card-body",
       )
     ),
-    div(
+    shiny::div(
       id = "instructions",
-      h3(icon("question"), "Instructions"),
-      div(
+      shiny::h3(shiny::icon("question"), "Instructions"),
+      shiny::div(
         class = "card-body",
         shiny::markdown("
            **To add an element:**
@@ -60,20 +77,20 @@ griditor_addin <- function() {
          ")
       )
     ),
-    div(
+    shiny::div(
       id = "elements",
-      h3(icon("layer-group"), "Added elements"),
-      div(
+      shiny::h3(shiny::icon("layer-group"), "Added elements"),
+      shiny::div(
         class = "card-body",
-        div(id = "added_elements")
+        shiny::div(id = "added_elements")
       )
     ),
-    div(
+    shiny::div(
       id = "editor",
-      div(
+      shiny::div(
         id = "editor-wrapper",
-        tags$svg(id = "editor-browser-header"),
-        uiOutput("grid_holder")
+        shiny::tags$svg(id = "editor-browser-header"),
+        shiny::uiOutput("grid_holder")
       )
     )
   )
@@ -93,7 +110,7 @@ griditor_addin <- function() {
       )
     )
 
-    observe({
+    bindEvent(observe({
       req(input$elements)
 
       grid_mat <- matrix(".",
@@ -103,16 +120,16 @@ griditor_addin <- function() {
         grid_mat[el$start_row:el$end_row, el$start_col:el$end_col] <- el$id
       }
 
-      layout_table <- to_md(
-        new_gridlayout(layout_mat = grid_mat,
-                       col_sizes = input$grid_sizing$cols,
-                       row_sizes = input$grid_sizing$rows,
-                       gap = input$grid_sizing$gap
+      layout_table <- gridlayout::to_md(
+        gridlayout::new_gridlayout(layout_mat = grid_mat,
+                                   col_sizes = input$grid_sizing$cols,
+                                   row_sizes = input$grid_sizing$rows,
+                                   gap = input$grid_sizing$gap
         )
       )
       rstudioapi::modifyRange(selected_range, layout_table, id = NULL)
       shiny::stopApp()
-    }) %>% bindEvent(input$get_code)
+    }), input$get_code)
   }
 
   # We'll use a pane viwer, and set the minimum height at
