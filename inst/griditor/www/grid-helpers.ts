97722e327197adce25c45205e066b97278f8639e
Nick Strayer <nick.strayer@gmail.com>
2021-02-24 16:55:24 -0500

    Template start/end fixed up

--- inst/griditor/www/grid-helpers.ts
+++ inst/griditor/www/grid-helpers.ts
@@ -1,14 +1,16 @@
 // Functions related to grid construction
 
 // Builds the start/end css string for a grid-{row,column}
-export function make_template_start_end([start, end]) {
+export function make_template_start_end(pos: Array<number>): string {
   // If we only have a single value just assume we take up one row
   // If single index is a negative one, we need to subtract instead of add to it
-  const negative_index = start < 0;
+  const start : number = pos[0];
+  const negative_index : boolean = start < 0;
 
   // Grid works with lines so if we want an element to end at the 4th column we
   // need to tell it to end at the (4+1)5th line, so we add one
-  end = end ? +end + 1 : start + (negative_index ? -1 : 1);
+  const end: number = pos[1] ? pos[1] + 1 : start + (negative_index ? -1 : 1);
+  // end = end ? +end + 1 : start + (negative_index ? -1 : 1);
 
   return `${start} / ${end}`;
 }
