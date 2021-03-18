d7ee57dd8999c9cef8f088574ed5ef7dcd7506be
Nick Strayer <nick.strayer@gmail.com>
2021-03-18 12:12:30 -0400

    Added basic webshot2 testing of layout

---
+++ tests/testthat/test-demo-apps.R
@@ -0,0 +1,16 @@
+# Run demo app and take a screenshot of landing page
+screenshot_demo_app <- function(app_loc) {
+  path <- tempfile(fileext = ".png")
+
+  webshot2::appshot(
+    app = app_loc,
+    file = path,
+    cliprect = "viewport"
+  )
+
+  path
+}
+
+test_that("geyser app demo", {
+  expect_snapshot_file(screenshot_demo_app(here::here("inst/demo_app/app.R")), "app.png")
+})
