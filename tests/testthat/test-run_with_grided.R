a08e942359458b17ca2bfec42f7ea4f53a97c4a3
Nick Strayer <nick.strayer@gmail.com>
2021-04-01 18:03:12 -0400

    Added a screenshot test of the run_with_grided results

--- tests/testthat/test-run_with_grided.R
+++ tests/testthat/test-run_with_grided.R
@@ -1,3 +1,5 @@
+source(here::here("tools/generate_screenshots.R"))
+
 test_that("Wont try and run on non grid_page apps", {
   expect_error(
     run_with_grided(shiny::fluidPage(
@@ -15,5 +17,17 @@ test_that("Wont try and run on non grid_page apps", {
     )),
     regexp = "run_with_grided needs to be used with an app that uses grid_page as its UI.",
     fixed = TRUE
+  )
+})
+
+
+test_that("geyser app", {
+  expect_snapshot_file(
+    screenshot_demo_app(
+      here::here('inst/realtime_layout_update/app.R'),
+      "geyser_w_grided.png",
+      force_rerun = TRUE
+    ),
+    "geyser-grided-app.png"
   )
 })
