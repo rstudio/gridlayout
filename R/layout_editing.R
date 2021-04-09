90e8417167e832b4977874532501ea51fd4c3f05
Nick Strayer <nick.strayer@gmail.com>
2021-04-09 11:38:56 -0400

    Table parsing is now even more robust. Fixes bug where two tables with no space between would get combined

--- R/layout_editing.R
+++ R/layout_editing.R
@@ -3,46 +3,62 @@
 # an element in the vector. For example this is the way the rstudioapi gives you
 # back the open-files context.
 find_layouts_in_file <- function(file_text){
-
   # This is a semi-complicated regex that looks for lines that appear to be
   # markdown table lines. This means they start with pipes and end with pipes.
   # Exceptions are made for the start and end which may contain a quote and
   # leading/trailing content respectively
 
-  # The rle() function gives us the sections of continuous table-ness which we
-  # can then parse through and try and read each table into a layout
-  md_table_lines <- rle(str_detect(file_text, '(^|\\")\\s*\\|.+\\|\\s*\\"*'))
+  table_lines <- str_detect(file_text, '(^|\\")\\s*\\|.+\\|\\s*\\"*')
+  start_lines <- str_detect(file_text, '(\'|\\")\\s*\\|.+\\|\\s*')
 
-  section_end <- cumsum(md_table_lines$lengths)
-  section_start <- section_end - md_table_lines$lengths + 1
+  all_tables <- list()
+  curr_table <- list()
+  prev_was_table <- FALSE
+  n_lines <- length(file_text)
+  for(i in seq_len(n_lines)){
+    is_table <- table_lines[i]
+    is_table_start <- start_lines[i]
+
+    last_line <- i == n_lines
+    curr_table_finished <- prev_was_table && (!is_table || is_table_start || last_line)
+    starting_new_table <- is_table && (is_table_start || !prev_was_table)
+
+    if (curr_table_finished) {
+      curr_table$end_row <- if(last_line) i else i - 1
+      curr_table$end_col <- pos_of_char(file_text[curr_table$end_row], "|", max)
+      all_tables[[length(all_tables) + 1]] <- curr_table
+      curr_table <- list()
+    }
+
+    if (starting_new_table) {
+      curr_table$start_row <- i
+      curr_table$start_col <- pos_of_char(file_text[i], "|", min)
+    }
+
+    prev_was_table <- is_table
+  }
+
 
   all_layouts <- lapply(
-    which(md_table_lines$values),
-    function(rle_index) {
-      start_row <- section_start[rle_index]
-      start_line <- file_text[start_row]
-      end_row <- section_end[rle_index]
-      end_line <- file_text[end_row]
+    all_tables,
+    function(t){
 
       # Get the position of the start of the table and end of the table
       # so we can perfectly extract it without any extraneous text like
       # quotes or assignment operators
-      start_col <- min(which(strsplit(start_line, split = "")[[1]] == "|"))
-      end_col <- max(which(strsplit(end_line, split = "")[[1]] == "|"))
-      table_text <- file_text[start_row:end_row]
-      table_text[1] <- substring(table_text[1], first = start_col)
-      n_row <- md_table_lines$lengths[rle_index]
-      table_text[n_row] <- substring(table_text[n_row], first = 1, last = end_col)
+      table_text <- file_text[t$start_row:t$end_row]
+      table_text[1] <- substring(table_text[1], first = t$start_col)
+      n_row <- length(table_text)
+      table_text[n_row] <- substring(table_text[n_row], first = 1, last = t$end_col)
 
       contents <- paste(table_text, collapse = "\n")
 
-      list(
-        start_row = start_row,
-        start_col = start_col,
-        end_row  = end_row,
-        end_col = end_col,
-        contents = contents,
-        layout = md_to_gridlayout(contents, null_instead_of_error = TRUE)
+      c(
+        t,
+        list(
+          contents = contents,
+          layout = md_to_gridlayout(contents, null_instead_of_error = TRUE)
+        )
       )
     }
   )
@@ -51,6 +67,12 @@ find_layouts_in_file <- function(file_text){
     x = all_layouts,
     f = function(x) !is.null(x$layout)
   )
+}
+
+
+pos_of_char <- function(string, char, collapse_fn = min){
+  all_chars <- strsplit(string, split = "")[[1]]
+  collapse_fn(which(all_chars == char))
 }
 
 
