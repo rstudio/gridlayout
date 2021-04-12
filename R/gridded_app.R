ca37e1d79cc19073b5d7c61681aae47cbfc1e607
Nick Strayer <nick.strayer@gmail.com>
2021-04-12 18:11:48 -0400

    The update layout button is now hidden if the rstudio api is not available

--- R/gridded_app.R
+++ R/gridded_app.R
@@ -1,98 +1,5 @@
-#' Run existing app in grided layout editing mode
-#'
-#'
-#' @param app Shiny app object as produced by \code{\link[shiny]{shinyApp()}}
-#'
-#' @return Shiny app object with original app encapuslated by grided editor
-#' @export
-#'
-run_with_grided <- function(app){
 
-  # Start by modifying the server code to respond to messages from the grided
-  # javascript
-  origServerFuncSource <- app[["serverFuncSource"]]
-
-  app[["serverFuncSource"]] <- function() {
-
-    origServerFunc <- origServerFuncSource()
-    function(input, output, session, ...) {
-
-      if (!"session" %in% names(formals(origServerFunc))) {
-        origServerFunc(input, output, ...)
-      } else {
-        origServerFunc(input, output, session, ...)
-      }
-
-      # Lets grided know it should send over initial app state
-      session$sendCustomMessage("shiny-loaded", 1)
-
-      current_layout <- reactive({
-        shiny::req(input$elements)
-        layout_from_grided(input$elements, input$grid_sizing)
-      })
-
-      initial_layout <- NULL
-      observe({
-        shiny::req(input$elements, input$grid_sizing)
-        if(is.null(initial_layout)){
-          initial_layout <<- layout_from_grided(input$elements, input$grid_sizing)
-        }
-      })
-
-      shiny::bindEvent(
-        shiny::observe({
-          layout_call <- paste(
-            "layout <- grid_layout_from_md(layout_table = \"",
-            "    ", to_md(current_layout()), "\")",
-            sep = "\n")
-
-          session$sendCustomMessage("code_modal", layout_call)
-        }),
-        input$get_code)
-
-      shiny::bindEvent(shiny::observe({
-        req(input$elements)
-        editor_selection <- rstudioapi::getSourceEditorContext()
-
-        layout_table <- Find(
-          x = find_layouts_in_file(editor_selection$contents),
-          f = function(x) layouts_are_equal(x$layout, initial_layout)
-        )
-
-        if(is.null(layout_table)){
-          warning("Could not find layout table to edit. Make sure your app script with layout definition is open in RStudio. Otherwise use the copy-layout button and manually change layout table.")
-        }
-
-        update_layout_in_file(editor_selection, layout_table, current_layout())
-
-        shiny::stopApp()
-      }), input$updated_code)
-    }
-  }
-
-  # Next we go into the UI and extract the body and place it within the grided
-  # UI
-
-  # Grab existing UI function out of app
-  existing_ui <- environment(app[["httpHandler"]])$ui
-
-  grid_page_tags <- find_grid_tags(existing_ui)
-
-  couldnt_find_tags <- is.null(grid_page_tags)
-  # This should be "grid_page" if we're looking at output of grid_page function
-  no_grid_page_div <- grid_page_tags[[1]][[2]]$attribs$id != "grid_page"
-  if(couldnt_find_tags || no_grid_page_div){
-    stop("run_with_grided needs to be used with an app that uses grid_page as its UI.")
-  }
-
-  # Replace the old ui with the new wrapped one
-  environment(app[["httpHandler"]])$ui <- grided_ui_wrapper(grid_page_tags, update_btn_text = "Finish editing")
-
-  app
-}
-
-
-grided_ui_wrapper <- function(grid_container, update_btn_text){
+grided_ui_wrapper <- function(grid_container, update_btn_text = NULL){
   shiny::tags$body(
     shiny::tags$head( shiny::tags$title("GridEd")),
     shiny::includeScript(
@@ -105,7 +12,7 @@ grided_ui_wrapper <- function(grid_container, update_btn_text){
                shiny::h2(shiny::HTML("GridEd<sub>(itor)</sub>: Build a grid layout for your Shiny app")),
                shiny::div(
                  class = "code_btns",
-                 shiny::actionButton("updated_code", update_btn_text),
+                 if (!is.null(update_btn_text)) shiny::actionButton("updated_code", update_btn_text),
                  shiny::actionButton("get_code", "Get layout code")
                )
     ),
---
+++ R/run_w_grided.R
@@ -0,0 +1,126 @@
+#' Run existing app in grided layout editing mode
+#'
+#'
+#' @param app Shiny app object as produced by \code{\link[shiny]{shinyApp()}}
+#'
+#' @return Shiny app object with original app encapuslated by grided editor
+#' @export
+#'
+run_with_grided <- function(app) {
+
+  # Start by modifying the server code to respond to messages from the grided
+  # javascript
+  origServerFuncSource <- app[["serverFuncSource"]]
+
+  # If we have access to the rstudio api we can enable editor manipulation.
+  # Otherwise we should hide the update button to not confuse the user.
+  in_rstudio <- requireNamespace("rstudioapi", quietly = TRUE) && rstudioapi::isAvailable()
+
+  app[["serverFuncSource"]] <- function() {
+    origServerFunc <- origServerFuncSource()
+    function(input, output, session, ...) {
+      if (!"session" %in% names(formals(origServerFunc))) {
+        origServerFunc(input, output, ...)
+      } else {
+        origServerFunc(input, output, session, ...)
+      }
+
+      # Lets grided know it should send over initial app state
+      session$sendCustomMessage("shiny-loaded", 1)
+
+      current_layout <- reactive({
+        shiny::req(input$elements)
+        layout_from_grided(input$elements, input$grid_sizing)
+      })
+
+      initial_layout <- NULL
+      observe({
+        shiny::req(input$elements, input$grid_sizing)
+        if (is.null(initial_layout)) {
+          initial_layout <<- layout_from_grided(input$elements, input$grid_sizing)
+        }
+      })
+
+      shiny::bindEvent(
+        shiny::observe({
+          send_layoutcall_popup(session, current_layout)
+        }),
+        input$get_code
+      )
+
+      shiny::bindEvent(shiny::observe({
+        req(input$elements)
+
+        if (in_rstudio) {
+          editor_selection <- rstudioapi::getSourceEditorContext()
+
+          layout_table <- Find(
+            x = find_layouts_in_file(editor_selection$contents),
+            f = function(x) layouts_are_equal(x$layout, initial_layout)
+          )
+        }
+
+        if (is.null(layout_table) || !in_rstudio) {
+          warning("Could not find layout table to edit. Make sure your app script with layout definition is open in RStudio. Otherwise use the copy-layout button and manually change layout table.")
+          send_layoutcall_popup(session, current_layout)
+        } else {
+          update_layout_in_file(editor_selection, layout_table, current_layout())
+          shiny::stopApp()
+        }
+
+      }), input$updated_code)
+    }
+  }
+
+  # Next we go into the UI and extract the body and place it within the grided
+  # UI
+
+  # Grab existing UI function out of app
+  existing_ui <- environment(app[["httpHandler"]])$ui
+
+  grid_page_tags <- find_grid_tags(existing_ui)
+
+  couldnt_find_tags <- is.null(grid_page_tags)
+  # This should be "grid_page" if we're looking at output of grid_page function
+  no_grid_page_div <- grid_page_tags[[1]][[2]]$attribs$id != "grid_page"
+  if (couldnt_find_tags || no_grid_page_div) {
+    stop("run_with_grided needs to be used with an app that uses grid_page as its UI.")
+  }
+
+  # Replace the old ui with the new wrapped one
+  environment(app[["httpHandler"]])$ui <- grided_ui_wrapper(
+    grid_page_tags,
+    update_btn_text = if (in_rstudio) "Finish editing"
+  )
+
+  app
+}
+
+
+send_layoutcall_popup <- function(session, current_layout){
+  layout_call <- paste(
+    "layout <- grid_layout_from_md(layout_table = \"",
+    "    ", to_md(current_layout()), "\")",
+    sep = "\n")
+
+  session$sendCustomMessage("code_modal", layout_call)
+}
+
+find_grid_tags <- function(x) {
+  is_shinytag <- inherits(x, "shiny.tag")
+  has_class <- is_shinytag && !is.null(x$attribs$class)
+
+  if (has_class && x$attribs$class == "container-fluid") {
+    x$children
+  } else if (is_shinytag | inherits(x, "list")) {
+    children <- if (is_shinytag) x[["children"]] else x
+    for (child in children) {
+      ret <- Recall(child)
+      if (!is.null(ret)) {
+        return(ret)
+      }
+    }
+  } else {
+    NULL
+  }
+}
