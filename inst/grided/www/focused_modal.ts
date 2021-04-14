b6f786aee4a8a1c05ae2954456778a32e4b10131
Nick Strayer <nick.strayer@gmail.com>
2021-04-14 19:35:33 -0400

    Bootstrap css messes up the modal so made class name more specific

--- inst/grided/www/focused_modal.ts
+++ inst/grided/www/focused_modal.ts
@@ -4,7 +4,7 @@ type Modal_Options = {
   background_callbacks?: Event_Listener | Event_Listener[];
   modal_callbacks?: Event_Listener | Event_Listener[];
   header_text?: string;
-}
+};
 export function focused_modal(opts: Modal_Options) {
   const background = make_el(
     document.querySelector("#grided__holder"),
@@ -14,12 +14,12 @@ export function focused_modal(opts: Modal_Options) {
     }
   );
 
-  const modal = make_el(background, "div.modal", {
+  const modal = make_el(background, "div.focused_modal", {
     event_listener: opts.modal_callbacks,
   });
 
   if (opts.header_text) {
-    make_el(modal, "div.modal_header", {
+    make_el(modal, "div.focused_modal_header", {
       innerHTML: opts.header_text,
       styles: {
         paddingBottom: "1rem",
