adee7fd7a2dde7b4f9a7f7b2cf0c3bbfcf94714b
Nick Strayer <nick.strayer@gmail.com>
2021-04-26 18:37:46 -0400

    Added some basic tests for alternate layouts

--- tests/testthat/test-new_gridlayout.R
+++ tests/testthat/test-new_gridlayout.R
@@ -107,3 +107,58 @@ test_that("Can initialize a layout with the element_list argument instead of a m
   )
 
 })
+
+test_that("Alternate layouts can be added for different sized screens", {
+
+  main_layout <- new_gridlayout(
+    layout_mat = matrix(c(
+      "header", "header",
+      "plota",  "plotb"
+    ), ncol = 2, byrow = TRUE),
+    col_sizes = c("1fr", "2fr"),
+    row_sizes = c("100px", "1fr"),
+    gap = "2rem"
+  )
+
+  mobile_layout <- new_gridlayout(
+    layout_mat = matrix(c(
+      "header",
+      "plota",
+      "plotb"
+    ), ncol = 1, byrow = TRUE),
+    col_sizes = c("1fr"),
+    row_sizes = c("100px", "400px", "500px"),
+    gap = "2rem"
+  )
+
+
+  main_w_alternate <- add_alternate_layout(
+    layout = main_layout,
+    alternate_layout = mobile_layout,
+    lower_bound_width = 400
+  )
+
+  expect_equal(
+    mobile_layout,
+    attr(main_w_alternate, "alternates")[[1]]$layout,
+    ignore_attr = TRUE
+  )
+
+  mobile_wo_plota <- new_gridlayout(
+    layout_mat = matrix(c(
+      "header",
+      "plotb"
+    ), ncol = 1, byrow = TRUE),
+    row_sizes = c("100px", "500px")
+  )
+
+  expect_error(
+    add_alternate_layout(
+      layout = main_layout,
+      alternate_layout = mobile_wo_plota,
+      lower_bound_width = 400
+    ),
+    regexp = "Layouts have mismatched elements: plota",
+    fixed = TRUE
+  )
+})
