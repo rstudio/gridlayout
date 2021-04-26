88e7a77480c0c9e96d4d189dd7a1c78d1264204c
Nick Strayer <nick.strayer@gmail.com>
2021-04-26 18:35:31 -0400

    Moved layout coercion helper to new_gridlayout script

--- R/md_to_gridlayout.R
+++ R/md_to_gridlayout.R
@@ -85,17 +85,6 @@ md_to_gridlayout <- function(layout_table, null_instead_of_error = FALSE){
 }
 
 
-# Function to allow layout being defined with markdown or with standard object
-coerce_to_layout <- function(layout_def){
-  if(inherits(layout_def, "character")){
-    # If we were passed a string directly then convert to a grid layout before
-    # proceeding
-    layout_def <- md_to_gridlayout(layout_def)
-  } else if(!inherits(layout_def, "gridlayout")){
-    stop("Passed layout must either be a markdown table or a gridlayout object.")
-  }
-  layout_def
-}
 
 
 
