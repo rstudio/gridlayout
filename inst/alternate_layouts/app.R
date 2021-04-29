785f42ad6549eda0a8fc9060d92c4be3bf193d97
Nick Strayer <nick.strayer@gmail.com>
2021-04-29 11:35:08 -0400

    Add alternate layouts demonstration app

---
+++ inst/alternate_layouts/app.R
@@ -0,0 +1,64 @@
+# The geyser app... but in grid!
+library(gridlayout)
+library(shiny)
+
+main_layout <- "
+|      |        |       |
+|------|--------|-------|
+|2rem  |200px   |1fr    |
+|80px  |header  |header |
+|1fr   |sidebar |plot   |"
+
+mobile_layout <- "
+|----- |--------|
+|2rem  |1fr     |
+|80px  |header  |
+|auto  |sidebar |
+|400px |plot    |"
+
+big_screen_layout <- "
+|-----|-------|--------|-----|
+|2rem |250px  | 250px  |1fr  |
+|1fr  |header |sidebar |plot |"
+
+my_layout <- new_gridlayout(
+  main_layout,
+  alternate_layouts = list(
+    list(
+      layout = mobile_layout,
+      upper_bound_width = 600,
+      container_height = "auto"
+    ),
+    list(
+      layout = big_screen_layout,
+      lower_bound_width = 1200
+    )
+  )
+)
+
+
+# The classic Geyser app with grid layout
+app <- shinyApp(
+  ui = grid_page(
+    layout = my_layout,
+    theme = bslib::bs_theme(),
+    use_bslib_card_styles = TRUE,
+    header = title_panel("This is my header"),
+    sidebar = grid_panel(
+      title = "Settings",
+      v_align = "center",
+      collapsable = TRUE,
+      sliderInput("bins","Number of bins:", min = 1, max = 50, value = 30, width = "100%")
+    ),
+    plot = plotOutput("distPlot", height = "100%")
+  ),
+  server = function(input, output) {
+    output$distPlot <- renderPlot({
+      x    <- faithful[, 2]
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+  }
+)
+app
+
