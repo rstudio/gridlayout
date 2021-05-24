0b85796f3333f73e357fdf35fa6f7313a8df6ab1
Nick Strayer <nick.strayer@gmail.com>
2021-05-24 17:27:44 -0400

    Updated filler text to explain itself

--- inst/grided/www/utils-misc.ts
+++ inst/grided/www/utils-misc.ts
@@ -151,7 +151,8 @@ export function overlap<Type>(a: Type[], b: Type[]) {
 
 export const filler_text = `
 <div class = "filler_text">
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  This filler text demonstrates how the height of an element spanning an "auto"
+  row is influenced by its content. While you're here...
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type 
   specimen book.
