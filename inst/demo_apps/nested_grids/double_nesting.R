619f6e370071f34dc14b53a36725c88db351fdb5
Nick Strayer <nick.strayer@gmail.com>
2021-05-28 17:59:54 -0400

    Made double nesting demo a recursive example for more thorough stress testing

--- inst/demo_apps/nested_grids/double_nesting.R
+++ inst/demo_apps/nested_grids/double_nesting.R
@@ -1,34 +1,53 @@
 library(gridlayout)
 library(shiny)
-
+library(fontawesome)
 my_layout <- "
-|     |1fr     |4fr    |
-|auto |header  |header |
-|4fr  |sidebar |nested |"
+|     |     |       |       |
+|-----|-----|-------|-------|
+|10px |1fr  |5fr   |1fr    |
+|1fr  |top  |top    |right  |
+|5fr |left |nested |right  |
+|1fr  |left |bottom |bottom |"
 
-# The classic Geyser app with grid layout
-app <- shinyApp(
+depth <- 4
+colors <- viridis::plasma(depth)
+
+emoji_panel <- function(emoji, level = 1){
+  grid_panel(
+    div(
+      style = htmltools::css(
+        "background-color" = colors[level],
+        width = "100%", height = "100%",
+        display = "grid",
+        color = "white",
+        "font-size" = "2rem",
+        "place-content" = "center"),
+      emoji
+    )
+  )
+}
+
+make_nested_panels <- function(level = 1) {
+  nested_grid_panel(
+    layout = my_layout,
+    elements = list(
+      top =    emoji_panel("↓", level),
+      bottom = emoji_panel("↑", level),
+      left =   emoji_panel("→", level),
+      right =  emoji_panel("←", level),
+      nested = if(level < depth) make_nested_panels(level + 1) else emoji_panel("🐢", level)
+    )
+  )
+}
+
+shinyApp(
   ui = grid_page(
     layout = my_layout,
-    header = title_panel("Level 1"),
-    sidebar = "I am a sidebar",
-    nested = nested_grid_panel(
-      layout = my_layout,
-      elements = list(
-        header = text_panel("level 2"),
-        sidebar = "I am a sidebar",
-        nested = nested_grid_panel(
-          layout = my_layout,
-          elements = list(
-            header = text_panel("level 3"),
-            sidebar = "I am a sidebar",
-            nested = "I am nested"
-          )
-        )
-      )
-    )
+    top =    emoji_panel("↓"),
+    bottom = emoji_panel("↑"),
+    left =   emoji_panel("→"),
+    right =  emoji_panel("←"),
+    nested = make_nested_panels()
   ),
-  server = function(input, output) {
-
-  }
+  server = function(input, output) {}
 )
