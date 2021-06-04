80755def8a379210e94c0ee8c4c6629a7eb19aad
Nick Strayer <nick.strayer@gmail.com>
2021-06-04 17:17:22 -0400

    Move template info generation into the grided functions script

--- inst/grided/template_chooser.R
+++ inst/grided/template_chooser.R
@@ -9,21 +9,34 @@ setwd(here("inst/grided/"))
 options(shiny.autoreload = TRUE)
 shiny::devmode(TRUE)
 
-classic <- gridlayout:::new_gridlayout("
-|      |        |       |
-|------|--------|-------|
-|2rem  |200px   |1fr    |
-|150px |header  |header |
-|1fr   |sidebar |plot   |")
-
-four_square <- gridlayout:::new_gridlayout("
-|     |1fr |1fr |
-|1fr  |A   | B  |
-|1fr  |C   | D  |")
-
-layout_templates = list(
-  dump_all_info(classic, "classic"),
-  dump_all_info(four_square, "four square")
+layout_templates <- list(
+  gen_template_info(
+    "|2rem  |200px   |1fr    |
+     |150px |header  |header |
+     |1fr   |sidebar |plot   |",
+    "classic",
+    flipped_els = c("sidebar")
+  ),
+  gen_template_info(
+    "|     |1fr |1fr |
+     |1fr  |A   | B  |
+     |1fr  |C   | D  |",
+    "four square"
+  ),
+  gen_template_info(
+    "|1rem |1fr       |1fr      |
+     |80px |header    |header   |
+     |2fr  |chickens  |chickens |
+     |1fr  |treePlot  |yarnPlot |",
+    "Focal Chart - Top"
+  ),
+  gen_template_info(
+    "|1rem |2fr      |1fr      |
+     |80px |header   |header   |
+     |1fr  |chickens |treePlot |
+     |1fr  |chickens |yarnPlot |",
+    "Focal Chart - Side"
+  )
 )
 
 shinyApp(
