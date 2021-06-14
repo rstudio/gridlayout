4cc5c746988a7794427844497f0a5e31e9952425
Nick Strayer <nick.strayer@gmail.com>
2021-06-14 14:56:25 -0400

    Made layout chooser a bit narrower

--- inst/grided/www/web-components/layout-gallery.ts
+++ inst/grided/www/web-components/layout-gallery.ts
@@ -24,7 +24,7 @@ export class LayoutGallery extends HTMLElement {
     <style>
       :host {
         display: block;
-        max-width: 1200px;
+        max-width: 1000px;
         margin-left: auto;
         margin-right: auto;
         padding-left: 1rem;
