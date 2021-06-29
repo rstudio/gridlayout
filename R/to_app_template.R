fe0fddd935bd30486961a563793f241e61cc1a72
Nick Strayer <nick.strayer@gmail.com>
2021-06-29 17:57:42 -0400

    Added new unexported function in to_app_template() that builds a template from the live_app function provided for the layout gallery

--- R/to_app_template.R
+++ R/to_app_template.R
@@ -8,23 +8,22 @@
 #' @return Character string of Shiny app that uses desired layout
 #' @export
 #'
-to_app_template <- function(layout) {
+to_app_template <- function(x) {
   UseMethod("to_app_template")
 }
 
 #' @export
-to_app_template.default <- function(layout){
+to_app_template.default <- function(x){
   cat("to_app_template generic")
 }
-
+paste_nl <- function(...){
+  paste(..., sep = "\n")
+}
 #' @export
-to_app_template.gridlayout <- function(layout){
-  paste_nl <- function(...){
-    paste(..., sep = "\n")
-  }
+to_app_template.gridlayout <- function(x){
   element_divs <- paste(
     lapply(
-      get_elements(layout),
+      get_elements(x),
       function(el){
         paste_nl(
           paste0("  ", el$id, " = grid_panel("),
@@ -35,11 +34,10 @@ to_app_template.gridlayout <- function(layout){
     ), collapse = ",\n")
 
   paste_nl(
-    "library(shiny)",
-    "library(gridlayout)",
+    "library(shiny)\nlibrary(gridlayout)",
     "",
     "app_layout <- \"",
-    to_md(layout),
+    to_md(x),
     "\"",
     "",
     "ui <- grid_page(",
@@ -51,6 +49,31 @@ to_app_template.gridlayout <- function(layout){
     "}",
     "",
     "shinyApp(ui, server)"
+  )
+}
+
+gallery_app_to_app_template <- function(layout_app){
+  if(!is.function(layout_app)){
+    stop("Layout app needs to be a function that returns a list of `ui` and `server`.")
+  }
+  # The first and last lines open and close the function and list bodies, so
+  # drop those. Drop an additional line at the end because the calling
+  # environment is printed as well
+  app_code <- vec_trim(capture.output({layout_app}), n_start = 2, n_end = 3)
+
+  # Figure out how many spaces indentation is so we can un-indent code. This
+  # looks for the first "u" because the first line (should) start "ui = ..."
+  n_spaces <- which(strsplit(app_code[1], split = "", fixed = TRUE)[[1]] == "u")[1] - 1
+
+  # Make sure we're indented two spaces in
+  app_code <- indent_text(app_code, -(n_spaces - 2))
+
+  # Wrap in a shiny app call with appropriate library calls and we're good!
+  paste_nl(
+    "library(shiny)\nlibrary(gridlayout)\n",
+    "shinyApp(",
+    paste(app_code, collapse = "\n"),
+    ")"
   )
 }
 
