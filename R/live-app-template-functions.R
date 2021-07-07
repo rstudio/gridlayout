f3713a176bb244945948d39e39d7050c73e2b9c6
Nick Strayer <nick.strayer@gmail.com>
2021-07-07 08:12:23 -0400

    Functionalized the four-square app

---
+++ R/live-app-template-functions.R
@@ -0,0 +1,73 @@
+# Various UI and Server components to built live-template apps. Comments of  "#'
+# start-app-scope" and "#' end-app-scope" denote code chunks that will be placed
+# outside of ui and server functions in the compiled app.
+panel_about_chick_weights <- function(){
+  grid_panel(
+    title = "About the ChickWeights dataset",
+    p("The body weights of the chicks were measured at birth and every second day thereafter until day 20.
+      They were also measured on day 21. There were four groups on chicks on different protein diets."),
+    v_align = "center"
+  )
+}
+
+panel_chooser_dropdowns <- function(){
+  #' start-app-scope
+  chicks_by_group <- by(data = ChickWeight$Chick, ChickWeight$Diet, FUN = unique)
+  names(chicks_by_group) <- paste("Diet:", names(chicks_by_group))
+  #' end-app-scope
+
+  grid_panel(
+    title = "Filters",
+    selectInput("diet", label = "Diet", choices = levels(ChickWeight$Diet)),
+    selectInput("chick", label = "Highlighted Chick", choices = chicks_by_group),
+    v_align = "center"
+  )
+}
+
+
+
+panel_weight_trajectories_for_diet <- function(){
+  grid_panel(
+    title = "Weight trajectories",
+    plotOutput("chickPlot")
+  )
+}
+
+weight_trajectories_for_diet <- function(input) {
+  renderPlot({
+    data_for_diet <- ChickWeight[ChickWeight$Diet == input$diet, ]
+
+    plot(
+      0, 0,
+      type = "n",
+      xlim = c(0, max(ChickWeight$Time)), xlab = "Time",
+      ylim = c(0, max(ChickWeight$weight)), ylab = "Weight (grams)"
+    )
+
+    for (id in unique(data_for_diet$Chick)) {
+      chick_data <- data_for_diet[data_for_diet$Chick == id, ]
+      lines(
+        x = chick_data$Time, y = chick_data$weight,
+        col = if (input$chick == id) "blue" else "grey"
+      )
+    }
+  })
+}
+
+
+panel_weight_dist_by_diet <- function(){
+  grid_panel(
+    title = "Weights by diet",
+    plotOutput("weightRanges")
+  )
+}
+
+weight_dist_by_diet <- function(input) {
+  renderPlot({
+    plot(
+      x = ChickWeight$Diet, xlab = "Diet",
+      y = ChickWeight$weight, ylab = "Weight (grams)",
+      ylim = c(0, max(ChickWeight$weight))
+    )
+  })
+}
--- inst/layout-templates/four-square.R
+++ inst/layout-templates/four-square.R
@@ -14,55 +14,19 @@ app_layout <- "
 "
 #' end-layout
 
-chicks_by_group <- by(data = ChickWeight$Chick, ChickWeight$Diet, FUN = unique)
-names(chicks_by_group) <- paste("Diet:", names(chicks_by_group))
 
 ui <- grid_page(
   layout = app_layout,
-  A = grid_panel(
-    title = "About the ChickWeights dataset",
-    p("The body weights of the chicks were measured at birth and every second day thereafter until day 20.
-      They were also measured on day 21. There were four groups on chicks on different protein diets."),
-    v_align = "center"
-  ),
-  B = grid_panel(
-    title = "Filters",
-    selectInput("diet", label = "Diet", choices = levels(ChickWeight$Diet)),
-    selectInput("chick", label = "Highlighted Chick", choices = chicks_by_group),
-    v_align = "center"
-  ),
-  C = grid_panel(
-    title = "Weights by diet",
-    plotOutput("weightRanges")
-  ),
-  D = grid_panel(
-    title = "Weight trajectories",
-    plotOutput("chickPlot")
-  )
+  A = panel_about_chick_weights(),
+  B = panel_chooser_dropdowns(),
+  C = panel_weight_dist_by_diet(),
+  D = panel_weight_trajectories_for_diet()
 )
 
 
 server <- function(input, output, session) {
-  time_range <- c(0, max(ChickWeight$Time))
-  weight_range <- c(0, max(ChickWeight$weight))
-
-  output$chickPlot <- renderPlot({
-    data_for_diet <- ChickWeight[ChickWeight$Diet == input$diet, ]
-
-    plot(0, 0, xlim = time_range, ylim = weight_range, type = "n", xlab = "Time", ylab = "Weight (grams)")
-
-    for (id in unique(data_for_diet$Chick)) {
-      chick_data <- data_for_diet[data_for_diet$Chick == id, ]
-      lines(
-        x = chick_data$Time, y = chick_data$weight,
-        col = if (input$chick == id) "blue" else "grey"
-      )
-    }
-  })
-
-  output$weightRanges <- renderPlot({
-    plot(ChickWeight$Diet, ChickWeight$weight, xlab = "Diet", ylab = "Weight (grams)", ylim = weight_range)
-  })
+  output$chickPlot <- weight_trajectories_for_diet(input)
+  output$weightRanges <- weight_dist_by_diet(input)
 }
 
 shinyApp(ui, server)
