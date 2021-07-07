4236cd9dfc5e00a9a3291d4320cfa80575c4a97a
Nick Strayer <nick.strayer@gmail.com>
2021-07-07 15:39:05 -0400

    Updated the addin version of the layout gallery with the new live-templates

--- R/layout_gallery.R
+++ R/layout_gallery.R
@@ -10,56 +10,53 @@ layout_gallery <- function(return_app_obj = FALSE){
     gen_template_info(
       "Classic",
       "|2rem  |200px   |1fr    |
-     |150px |header  |header |
-     |1fr   |sidebar |plot   |",
+       |150px |header  |header |
+       |1fr   |sidebar |plot   |",
      app_loc = "classic.R"
     ),
     gen_template_info(
       "Four-Square",
-      "|     |    |    |
-     |-----|----|----|
-     |1rem |1fr |1fr |
-     |1fr  |A   |B   |
-     |1fr  |C   |D   |"
+      "|1rem |1fr |1fr |
+       |1fr  |A   |B   |
+       |1fr  |C   |D   |",
+     app_loc = "four-square.R"
     ),
     gen_template_info(
       "Focal Chart - Top",
       "|1rem |1fr       |1fr      |
-     |80px |header    |header   |
-     |2fr  |chickens  |chickens |
-     |1fr  |treePlot  |yarnPlot |"
+       |80px |header    |header   |
+       |2fr  |mainPlot  |mainPlot |
+       |1fr  |controls  |subPlot  |",
+     app_loc = "focal-chart-top.R"
     ),
     gen_template_info(
       "Focal Chart - Side",
       "|1rem |2fr      |1fr      |
-     |80px |header   |header   |
-     |1fr  |chickens |treePlot |
-     |1fr  |chickens |yarnPlot |"
+       |80px |header   |header   |
+       |1fr  |mainPlot |controls |
+       |1fr  |mainPlot |subPlot  |",
+     app_loc = "focal-chart-side.R"
     ),
     gen_template_info(
       "Stack",
-      "|      |         |
-     |------|---------|
-     |1rem  |1fr      |
-     |80px  |header   |
-     |1fr   |chickens |
-     |1fr   |treePlot |
-     |1fr   |yarnPlot |
-     |1fr   |stockTable |"
+      "|1rem |1fr      |
+       |80px |header   |
+       |1fr  |controls |
+       |1fr  |mainPlot |
+       |1fr  |subPlot  |",
+     app_loc = "stack.R"
     ),
     gen_template_info(
       "Scrolling-Stack",
-      "|      |           |
-     |------|-----------|
-     |1rem  |1fr        |
-     |80px  |header     |
-     |400px |chickens   |
-     |400px |treePlot   |
-     |400px |yarnPlot   |
-     |400px |stockTable |"
+      "|1rem  |1fr      |
+       |80px  |header   |
+       |400px |controls |
+       |400px |mainPlot |
+       |400px |subPlot  |
+       |400px |about    |",
+     app_loc = "scrolling-stack.R"
     )
   )
-
 
   grided_app(starting_layout = layout_templates, return_app_obj = return_app_obj)
 }
