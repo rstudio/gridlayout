f9e10bc82f5742bad1c9758a1e3e0de4320be25c
Nick Strayer <nick.strayer@gmail.com>
2022-05-10 18:52:14 -0400

    Simplified a little bit

--- inst/demo_apps/simple/app.R
+++ inst/demo_apps/simple/app.R
@@ -1,10 +1,12 @@
 # Experiment with how little we can do but still get layout
+
+# Make sure you have latest dev version of gridlayout
+# pak::pkg_install('rstudio/gridlayout')
 library(gridlayout)
 library(shiny)
 library(bslib)
 
-# Take whatever tag we have and put it in a given grid area and add an
-# additional class to it
+# The minimal adornment of a tag to be grid-aware and always able to be classed
 panel <-  function(area, ..., tag = tags$div, additional_class = "my-card"){
   p <- tag(...)
   p$attribs$style <- paste0(p$attribs$style, "grid-area:", area, ";")
@@ -16,13 +18,11 @@ theme <- bs_theme() |>
     rules = list(
       ".my-panel {
         --panel-gap: 12px;
-        --pad: 5px;
+        --pad: 8px;
         margin: var(--panel-gap);
         padding: var(--pad);
       }",
-      ".my-card {
-        background-color: white;
-      }",
+      ".my-card { background-color: white; }",
       ".left-sidebar {
         --panel-gap: 0;
         background-color: $gray-400;
@@ -32,16 +32,14 @@ theme <- bs_theme() |>
         background-color: $primary;
         color: $gray-100;
       }",
-      ".shiny-plot-output {
+      "#distPlot {
         --pad: 0;
         max-width: calc(100%  - 2*var(--panel-gap));
         max-height: calc(100% - 2*var(--panel-gap));
-        min-width: 0;
-        min-height: 0;
       }",
       ".container-fluid {
         padding: 0px;
-        background-color: $gray-100;
+        background-color: $gray-200;
       }"
     )
   )
