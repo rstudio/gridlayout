cff3082eedd3149a82c733c8a6d4adc477531c64
Nick Strayer <nick.strayer@gmail.com>
2022-05-23 14:38:31 -0400

    Export the panel function

--- R/simple_panel.R
+++ R/simple_panel.R
@@ -1,4 +1,17 @@
-# The minimal adornment of a tag to be grid-aware and always able to be classed
+#' Simple grid panel
+#'
+#' The minimal adornment of a tag to be grid-aware and always able to be classed
+#'
+#' @param area Area of grid corresponding to the wrapping grid containers grid
+#'   definition
+#' @param ... Arguments passed onto the `tag` used. See `tag` argument
+#' @param tag Tag used to build card. Defaults to `htmltools::div()` but can be
+#'   any function that generates a html tag as output. E.g. `shiny::plotOutput`.
+#' @param class Any additional classes you may want to add to your panel
+#'
+#' @return
+#' @export
+#'
 panel <-  function(area, ..., tag = tags$div, class = ""){
   p <- tag(...)
   p$attribs$style <- paste0(p$attribs$style, "grid-area:", area, ";")
