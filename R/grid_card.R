cc03ffdac567bc8d0cf5ea4051f60eb6b2e794fe
Nick Strayer <nick.strayer@gmail.com>
2022-06-15 17:13:13 -0400

    Move all the card related functions into the same script and add a new version of nested to the mix

--- R/grid_card.R
+++ R/grid_card.R
@@ -14,7 +14,102 @@ grid_card <- function(area, ...){
 }
 
 
+#' Grid Plot Output
+#'
+#' A simple plot-on-the-grid function. For more control - such as adding headers
+#' or footers - you should use [`grid_card()`] and [`bslib::card_plot_output()`]
+#' directly.
+#'
+#'
+#' @inheritParams grid_panel
+#' @param outputId Output id of the plot output. Used to link to server code
+#'   generating plot. If left unset this will use the same value as the `area`
+#'   argument.
+#' @inheritDotParams bslib::card_plot_output
+#'
+#' @return A grid panel filled with plot output
+#' @export
+#'
+#'
+grid_plot <- function(
+    area,
+    outputId = area,
+    ...
+) {
+  grid_card(
+    area = area,
+    card_plot_output(outputId = outputId, ...)
+  )
+}
 
+
+#' Grid panel with nested gridlayout
+#'
+#' Creates a panel for a layout with its own internal gridlayout
+#'
+#' @inheritParams grid_panel
+#' @inheritParams grid_container
+#'
+#' @seealso [grid_panel], [grid_container]
+#'
+#' @return A `grid_panel` with a nested layout within it
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#' library(gridlayout)
+#' library(shiny)
+#'
+#' # A centered text panel
+#' grid_panel_text_c <- function(...) grid_panel_text(..., h_align="center")
+#'
+#' my_layout <- "
+#' |A |B |
+#' |C |D |"
+#' shinyApp(
+#'   ui = grid_page(
+#'     layout = my_layout,
+#'     grid_panel_text_c("A","A"),
+#'     grid_panel_text_c("B","B"),
+#'     grid_panel_text_c("C","C"),
+#'     grid_panel_nested(
+#'       "D",
+#'       layout = my_layout,
+#'       grid_panel_text_c("A","A2"),
+#'       grid_panel_text_c("B","B2"),
+#'       grid_panel_text_c("C","C2"),
+#'       grid_panel_text_c("D","D2")
+#'     )
+#'   ),
+#'   server = function(input, output) {}
+#' )
+#' }
+grid_nested <- function(
+    area,
+    layout,
+    ...,
+    id = NULL,
+    title = NULL,
+    use_bslib_card_styles = FALSE,
+    flag_mismatches = TRUE
+){
+
+  grid_card(
+    area = area,
+    if (!is.null(title)) bslib::card_header(title),
+    bslib::card_body(
+      grid_container(
+        layout = new_gridlayout(layout, container_height = "100%"),
+        use_bslib_card_styles = use_bslib_card_styles,
+        flag_mismatches = flag_mismatches,
+        id = id,
+        ...
+      ),
+      stretch = TRUE
+    )
+  )
+
+}
 
 
 
--- inst/demo_apps/nested_grids/app.R
+++ inst/demo_apps/nested_grids/app.R
@@ -4,63 +4,55 @@ library(gridlayout)
 library(shiny)
 library(bslib)
 
-my_layout <- "
-|20px |1fr     |1fr     |
-|auto |header  |header  |
-|1fr  |nestedA |nestedB |"
-
-content_layout <- "
-|20px |auto     |1fr         |
-|auto |icon     |bin_chooser |
-|1fr  |settings |plot        |"
-
-R_logo <- grid_card(area = "icon", fontawesome::fa("r-project"))
-
+drawHist <- function(nbins, color) {
+  x    <- faithful[, 2]
+  bins <- seq(min(x), max(x), length.out = nbins + 1)
+  hist(x, breaks = bins, col = color, border = 'white')
+}
 # The classic Geyser app with grid layout
 app <- shinyApp(
   ui = grid_page(
-    layout = my_layout,
-    theme = bslib::bs_theme(),
-    grid_place("header", tags$h1("Nested Grids")),
-    grid_panel_nested(
-      area = "nestedA",
-      layout = content_layout,
-      R_logo,
-      grid_card(
-        "bin_chooser",
-        sliderInput("bins", label = "Number of bins", min = 1, max = 50, value = 30)
-      ),
-      grid_plot("plot", "distPlot"),
-      grid_card(
-        "settings",
-        textOutput('current_bin_num')
-      )
+    layout = c(
+      "header  header",
+      "sidebar plots"
     ),
-    grid_panel_nested(
-      area = 'nestedB',
-      title = "Nested within a titled panel",
-      layout = content_layout,
-      R_logo,
-      grid_card("bin_chooser", "Bin Chooser L1"),
-      grid_card("settings", "Settings L1"),
-      grid_panel_nested(
-        "plot",
-        layout = content_layout,
-        R_logo,
-        grid_card("bin_chooser", "Bin Chooser L2"),
-        grid_card("settings", "Settings L2"),
-        grid_card("plot", "Plot")
-      )
+    row_sizes = c("50px", "1fr"),
+    col_sizes = c("250px", "1fr"),
+    gap_size = "10px",
+    theme = bslib::bs_theme(),
+    grid_header("header", "This is my header"),
+    grid_card(
+      "sidebar",
+      card_header("Settings"),
+      sliderInput("bins","Number of bins:", min = 1, max = 50, value = 30, width = "100%")
+    ),
+    grid_nested(
+      'plots',
+      title = "Plots - in technicolor",
+      layout = c(
+        "distPlot distPlot distPlot",
+        "redPlot  bluePlot greenPlot"
+      ),
+      grid_plot('distPlot'),
+      grid_plot('redPlot'),
+      grid_plot('bluePlot'),
+      grid_plot('greenPlot')
+
     )
   ),
   server = function(input, output) {
     output$distPlot <- renderPlot({
-      x    <- faithful[, 2]
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      drawHist(input$bins, "darkgray")
     })
-    output$current_bin_num <- renderText({
-      paste("Histogram has", input$bins, "bins.")
+    output$redPlot <- renderPlot({
+      drawHist(input$bins, "orangered")
+    })
+    output$bluePlot <- renderPlot({
+      drawHist(input$bins, "steelblue")
+    })
+    output$greenPlot <- renderPlot({
+      drawHist(input$bins, "forestgreen")
     })
   }
 )
+app
