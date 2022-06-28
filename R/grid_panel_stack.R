51c6f90dbfd395852370540735ce27e08b8cf238
Nick Strayer <nick.strayer@gmail.com>
2022-06-28 11:41:57 -0400

    Swap main panel doc string to panel_stack

--- R/grid_panel_stack.R
+++ R/grid_panel_stack.R
@@ -3,7 +3,34 @@
 #' Contain a series of ui elements with each vertically stacked on top of
 #' eachother. Internally uses css flexbox to align items.
 #'
-#' @inheritParams grid_panel
+#' @param area Name of grid area, should match an area defined in the layout
+#'   section of the wrapping `grid_page()` or `grid_container()`.
+#' @param ... Elements to include within the panel
+#' @param title Character string to go across top of panel with label. If left
+#'   blank the card contents will take up entire space.
+#' @param use_card_styles Should the element contained in panel be made to look
+#'   like an enclosed card?
+#' @param collapsible Should the card be able to be collapsed (`TRUE` or
+#'   `FALSE`)? Gridlayout will only show collapser if the layout allows it
+#'   (panel is entirely positioned within "auto" sized rows, and has a title).
+#'   Setting this to `FALSE` will mean collapsibility of the panel will never be
+#'   enabled, regardless of layout.
+#' @param scrollable Should scroll-bars be added so content that is larger than
+#'   the panel can be seen?
+#' @param v_align,h_align Vertical and Horizontal alignment of content in panel.
+#'   Options include `"center", "start", "end", "stretch"`. These are a direct
+#'   mapping to the the [css-spec for
+#'   `justify-items`](https://developer.mozilla.org/en-US/docs/Web/CSS/justify-items)
+#'    (=`h_align`) and
+#'   [`align-items`](https://developer.mozilla.org/en-US/docs/Web/CSS/align-items)
+#'    (= `v_align`).
+#' @param panel_id ID of `grid_panel` div. Usually this is automatically set by
+#'   `grid_page()` or `grid_container()`.
+#' @param panel_class Class(es) of `grid_panel` div. Like `panel_id` this is
+#'   typically automatically generated and should only be touched in advanced
+#'   usage.
+#' @param padding A valid css size that overrides the default of how much
+#'   spacing is around the card contents
 #' @param item_gap How much space should there be between consecutive items?
 #' @param item_alignment How should the items within the panel be aligned.
 #'   Defaults to stacking downward from the top. Options include `"top"`,
